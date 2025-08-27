d7b6dfe164abfa675fd3eae41a02e9b3f7f73c57
davidperalta <davidperalta@gmail.com>
2025-08-27 14:27:29 -0600

    Updates

---
+++ assets/AboutView-B0-ftp5p.js
@@ -0,0 +1,1 @@
+import{_ as e,c as o,o as t}from"./index-BmSmVutR.js";const c={name:"AboutView"};function n(r,s,a,_,p,u){return t(),o("h1",null,"About")}const i=e(c,[["render",n]]);export{i as default};
