c46e02f0b70d5f4652ed1c05bd469d0d570643c6
DavidPeralta <davidperalta@gmail.com>
2025-08-28 15:46:52 -0600

    se sube a git hub pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vuetify from 'vite-plugin-vuetify'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    vuetify({ autoImport: true })
+  ],
+  base: '/ortopediaApp/',
   server: {
     allowedHosts: ['.ngrok-free.app'],
     port: 4000
