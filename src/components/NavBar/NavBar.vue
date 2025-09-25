eb233341e8584679560f770608e510c29ec9e807
David Peralta Arenal <davidperalta@unindetec.edu.mx>
2025-09-25 15:43:06 -0600

    feat(navbar):se agrega modal e informacion del item Nosotros

--- src/components/NavBar/NavBar.vue
+++ src/components/NavBar/NavBar.vue
@@ -6,7 +6,7 @@
 
     <template v-if="$vuetify.display.mdAndUp">
       <div style="display: flex; gap: 30px; justify-content: center; align-items: center;">
-        <v-toolbar-subtitle>Nosotros</v-toolbar-subtitle>
+        <v-toolbar-subtitle @click="abrirDialogNosotros()">Nosotros</v-toolbar-subtitle>
         <v-toolbar-subtitle>Productos</v-toolbar-subtitle>
         <v-toolbar-subtitle>Contacto</v-toolbar-subtitle>
       </div>
@@ -37,9 +37,9 @@
   <v-navigation-drawer v-model="drawerUserCarrito" temporary location="right"
     style="height: auto; max-height: fit-content; ">
     <v-list>
-       <v-list-item prepend-icon="mdi-information" title="Nosotros"></v-list-item>
-        <v-list-item prepend-icon="mdi-store" title="Productos"></v-list-item>
-         <v-list-item prepend-icon="mdi-account-box" title="Contacto"></v-list-item>
+      <v-list-item prepend-icon="mdi-information" title="Nosotros" @click="abrirDialogNosotros()"></v-list-item>
+      <v-list-item prepend-icon="mdi-store" title="Productos"></v-list-item>
+      <v-list-item prepend-icon="mdi-account-box" title="Contacto"></v-list-item>
 
       <v-list-item prepend-icon="mdi-cart-variant" title="Carrito"></v-list-item>
       <v-list-item prepend-icon="mdi-account-circle" title="Cuenta"></v-list-item>
@@ -48,10 +48,110 @@
 
   <v-main>
   </v-main>
+
+
+  <!-- Dialog Conpartir Link de Dispositivo-->
+  <v-dialog v-model="dialogNosotros" width="auto" max-width="850" persistent transition="dialog-bottom-transition">
+    <v-card  prepend-icon="mdi-share-variant-outline" title="Nosotros">
+      <v-divider></v-divider>
+      <v-card>
+        <v-tabs v-model="tab" align-tabs="center" bg-color="blue-darken-1" stacked>
+          <v-tab value="tab-1">
+            <v-icon icon="mdi-phone"></v-icon>
+
+            Proposito
+          </v-tab>
+
+          <v-tab value="tab-2">
+            <v-icon icon="mdi-heart"></v-icon>
+
+            Personalidad
+          </v-tab>
+
+          <v-tab value="tab-3">
+            <v-icon icon="mdi-account-box"></v-icon>
+
+            Servicio
+          </v-tab>
+        </v-tabs>
+
+        <v-tabs-window v-model="tab">
+          <v-tabs-window-item value="tab-1">
+            <v-card>
+              <v-card-text style="margin: 16px;">
+                <h4 style="margin-bottom: 15px;">COMPROMETIDO, VERSATIL Y EFICIENTE.</h4>
+                <br>
+                <v-row>
+                  <v-col cols="12" md="4">
+                    <img src="" alt="img proposito">
+                  </v-col>
+
+                  <v-col cols="12" md="8">
+                    <p>{{ text }}</p>
+                  </v-col>
+                </v-row>
+              </v-card-text>
+            </v-card>
+          </v-tabs-window-item>
+
+          <v-tabs-window-item value="tab-2">
+            <v-card>
+              <v-card-text style="margin: 16px;">
+                <p style="font-size: 16px; font-weight: bold;">
+                  Somos un aliado medico, una persona confiable y organizada, resolutiva, cercana pero profesional, flexible e
+                  imparcial, claro y directo, transparentes y comprometidos.
+                </p >
+                <br>
+                <v-row>
+                  <v-col cols="12" md="4">
+                    <img src="" alt="img personalidad">
+                  </v-col>
+
+                  <v-col cols="12" md="8">
+                    <ol>
+                  <li>OFRECEMOS UN CATALOGO ABIERTO Y FLEXIBLE POR DEMANDA</li>
+                  <li>PLATAFORMA DIGITAL CON COTIZACION INMEDIATA Y COMUNICACION</li>
+                  <li>SERVICIO DE ASESORIA PERSONALIZADA</li>
+                  <li>ENTREGA EXPRESS</li>
+                  <li>PROGRAMAS DE ABASTECIMIENTO AUTOMATICO</li>
+                </ol>
+                  </v-col>
+                </v-row>
+                
+              </v-card-text>
+            </v-card>
+          </v-tabs-window-item>
+
+          <v-tabs-window-item value="tab-3">
+            <v-card>
+              <v-card-text style="margin: 16px;">
+
+              </v-card-text>
+            </v-card>
+          </v-tabs-window-item>
+        </v-tabs-window>
+      </v-card>
+
+      <v-divider></v-divider>
+      <v-spacer></v-spacer>
+      <div style="display: flex;flex-wrap: wrap;justify-content: space-between;margin: 8px;">
+        <div>
+          <v-btn text="Cerrar" color="deep-orange-accent-4" variant="tonal" @click="dialogNosotros = false"></v-btn>
+        </div>
+      </div>
+    </v-card>
+  </v-dialog>
+
 </template>
 
 <script setup>
 import { ref, watch } from 'vue'
+
+const dialogNosotros = ref(false);
+
+const tab = ref(null)
+
+const text = 'DISTRIBUIR Y PROVEER DE ALTA VARIEDAD DE INSUMOS MEDICOS Y MATERIAL MEDICO DE ALTA CALIDAD, CON EL OBJETIVO DE SATISFACER CUALQUIER NECESIDAD Y VOLVERSE INDISPENSABLE PARA LAS INSTITUCIONES DE SALUD, PROFESIONALES DEL SECTOR Y PACIENTES.'
 
 const items = [
   {
@@ -79,6 +179,11 @@ const group = ref(null)
 watch(group, () => {
   drawer.value = false
 })
+
+
+const abrirDialogNosotros = () => {
+  dialogNosotros.value = true;
+}
 </script>
 
 <style scoped>
@@ -98,7 +203,7 @@ watch(group, () => {
   color: #344767 !important;
 }
 
-v-toolbar-subtitle{
+v-toolbar-subtitle {
   cursor: pointer;
 }
 </style>
