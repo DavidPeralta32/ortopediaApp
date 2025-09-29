264646d1cd1a02bf6172ae31970ee222c02ec9c5
David Peralta <davi32peralta@gmail.com>
2025-09-29 13:43:26 -0600

    feat(Main): se agrega webHasHistory para historial de router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,27 +1,15 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import type { RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'Home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: () => import('../views/AboutView.vue') // Lazy load
-  },
-  {
-  path: '/productos',
-  name: 'Productos',
-  component: () => import('../views/Productos/Productos.vue')
-}
+  { path: '/', name: 'Home', component: HomeView },
+  { path: '/about', name: 'About', component: () => import('../views/AboutView.vue') },
+  { path: '/productos', name: 'Productos', component: () => import('../views/Productos/Productos.vue') }
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL), // ← Hash mode
   routes
 })
 
