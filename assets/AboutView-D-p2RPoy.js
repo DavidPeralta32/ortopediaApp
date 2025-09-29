35316761c35061fea77be295362ab92499daeb19
David Peralta <davi32peralta@gmail.com>
2025-09-29 13:42:03 -0600

    Updates

--- assets/AboutView-D-p2RPoy.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as o,o as t}from"./index-QMCH-2XT.js";const c={name:"AboutView"};function n(r,s,a,_,p,u){return t(),o("h1",null,"About")}const i=e(c,[["render",n]]);export{i as default};
