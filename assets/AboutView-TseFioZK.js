339dade9144bce107f0043a178f8c7cd36638924
David Peralta <davi32peralta@gmail.com>
2025-09-29 13:10:26 -0600

    Updates

--- assets/AboutView-TseFioZK.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as o,o as t}from"./index-w99erXsG.js";const c={name:"AboutView"};function n(r,s,a,_,p,u){return t(),o("h1",null,"About")}const i=e(c,[["render",n]]);export{i as default};
